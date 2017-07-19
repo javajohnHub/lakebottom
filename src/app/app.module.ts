61deb8a2c26436a8cd2ddd322ad52b56198969f3
javajohnhub <joedw1978@gmail.com>
2017-07-19 12:31:38 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AlertModule, ButtonsModule, CarouselModule, CollapseModule, DropdownModule } from 'ng2-bootstrap/ng2-bootstrap';
+import {Boot419RoutingModule} from './app-routing.module';
+import {Ng2PageScrollModule} from 'ng2-page-scroll';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IframesComponent } from './iframes/iframes.component';
+import { AlbumsComponent } from './albums/albums.component';
 
+import { FooterComponent } from './footer/footer.component';
+import { FacebookComponent } from './facebook/facebook.component';
+import { WhyComponent } from './why/why.component';
+import { ContactComponent } from './contact/contact.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    IframesComponent,
+    AlbumsComponent,
+    FooterComponent,
+    FacebookComponent,
+    WhyComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AlertModule,
+    ButtonsModule,
+    CarouselModule,
+    CollapseModule,
+    DropdownModule,
+    Boot419RoutingModule,
+    Ng2PageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
